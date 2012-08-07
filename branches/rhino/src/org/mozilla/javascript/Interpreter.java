d73d8e2c8fc352bf0887e5758d242cce29bda454
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2012-08-07 16:53:54 +0000

    more fixes
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code@7205 5f5364db-9458-4db8-a492-e30667be6df6

--- branches/rhino/src/org/mozilla/javascript/Interpreter.java
+++ branches/rhino/src/org/mozilla/javascript/Interpreter.java
@@ -2842,7 +2842,11 @@ switch (op) {
     private static void enterFrame(Context cx, CallFrame frame, Object[] args,
                                    boolean continuationRestart)
     {
-        boolean usesActivation = frame.idata.itsNeedsActivation;
+       if (frame.parentFrame != null && !frame.parentFrame.fnOrScript.isScript()) {
+           frame.fnOrScript.defaultPut("caller", frame.parentFrame.fnOrScript);
+       }
+
+       boolean usesActivation = frame.idata.itsNeedsActivation;
         boolean isDebugged = frame.debuggerFrame != null;
         if(usesActivation || isDebugged) {
             Scriptable scope = frame.scope;
@@ -2891,6 +2895,8 @@ switch (op) {
     private static void exitFrame(Context cx, CallFrame frame,
                                   Object throwable)
     {
+        frame.fnOrScript.delete("caller");
+
         if (frame.idata.itsNeedsActivation) {
             ScriptRuntime.exitActivationFunction(cx);
         }
