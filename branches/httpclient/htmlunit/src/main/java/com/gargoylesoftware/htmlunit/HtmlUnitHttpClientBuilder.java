56890c45e72306214ace450dcedf411b8f082b5e
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2014-01-24 06:03:11 +0000

    branch: fix HtmlUnitHttpClientBuilder
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code@9053 5f5364db-9458-4db8-a492-e30667be6df6

--- branches/httpclient/htmlunit/src/main/java/com/gargoylesoftware/htmlunit/HtmlUnitHttpClientBuilder.java
+++ branches/httpclient/htmlunit/src/main/java/com/gargoylesoftware/htmlunit/HtmlUnitHttpClientBuilder.java
@@ -16,13 +16,20 @@ package com.gargoylesoftware.htmlunit;
 
 import java.lang.reflect.Field;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
 import org.apache.http.config.ConnectionConfig;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.TextUtils;
 
 /**
  * A helper class for configuring {@link HttpClientBuilder}.
@@ -35,12 +42,38 @@ class HtmlUnitHttpClientBuilder {
     public static void configureConnectionManager(final HttpClientBuilder builder) {
         final ConnectionSocketFactory socketFactory = new SocksConnectionSocketFactory();
         
-        final LayeredConnectionSocketFactory sslSocketFactory = (LayeredConnectionSocketFactory) getField(builder, "sslSocketFactory");
+        LayeredConnectionSocketFactory sslSocketFactory = (LayeredConnectionSocketFactory) getField(builder, "sslSocketFactory");
         final SocketConfig defaultSocketConfig = (SocketConfig) getField(builder, "defaultSocketConfig");
         final ConnectionConfig defaultConnectionConfig = (ConnectionConfig) getField(builder, "defaultConnectionConfig");
         final boolean systemProperties = (Boolean) getField(builder, "systemProperties");
         final int maxConnTotal = (Integer) getField(builder, "maxConnTotal");
         final int maxConnPerRoute = (Integer) getField(builder, "maxConnPerRoute");
+        X509HostnameVerifier hostnameVerifier = (X509HostnameVerifier) getField(builder, "hostnameVerifier");
+        final SSLContext sslcontext = (SSLContext) getField(builder, "sslcontext");
+
+        if (sslSocketFactory == null) {
+            final String[] supportedProtocols = systemProperties ? split(
+                    System.getProperty("https.protocols")) : null;
+            final String[] supportedCipherSuites = systemProperties ? split(
+                    System.getProperty("https.cipherSuites")) : null;
+            if (hostnameVerifier == null) {
+                hostnameVerifier = SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
+            }
+            if (sslcontext != null) {
+                sslSocketFactory = new SSLConnectionSocketFactory(
+                        sslcontext, supportedProtocols, supportedCipherSuites, hostnameVerifier);
+            } else {
+                if (systemProperties) {
+                    sslSocketFactory = new SSLConnectionSocketFactory(
+                            (SSLSocketFactory) SSLSocketFactory.getDefault(),
+                            supportedProtocols, supportedCipherSuites, hostnameVerifier);
+                } else {
+                    sslSocketFactory = new SSLConnectionSocketFactory(
+                            SSLContexts.createDefault(),
+                            hostnameVerifier);
+                }
+            }
+        }
 
         final PoolingHttpClientConnectionManager poolingmgr = new PoolingHttpClientConnectionManager(
                 RegistryBuilder.<ConnectionSocketFactory>create()
@@ -69,6 +102,13 @@ class HtmlUnitHttpClientBuilder {
             poolingmgr.setDefaultMaxPerRoute(maxConnPerRoute);
         }
         builder.setConnectionManager(poolingmgr);
+    }
+
+    private static String[] split(final String s) {
+        if (TextUtils.isBlank(s)) {
+            return null;
+        }
+        return s.split(" *, *");
     }
 
     private static Object getField(final HttpClientBuilder builder, final String fieldName) {
