7acbffd7424066f17ec2c45aff5aefc739c13206
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2014-02-17 11:03:10 +0000

    Release 2.14
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code@9134 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ tags/HtmlUnit-2.14/src/test/java/com/gargoylesoftware/htmlunit/html/HtmlImageInput2Test.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2002-2014 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.html;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+
+/**
+ * Tests for {@link HtmlImageInput}.
+ *
+ * @version $Revision$
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+public class HtmlImageInput2Test extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "-", "-", "-" })
+    public void defaultValues() throws Exception {
+        final String html = "<html><head><title>foo</title>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var input = document.getElementById('image1');\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+
+            + "    input = document.createElement('input');\n"
+            + "    input.type = 'image';\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+
+            + "    var builder = document.createElement('div');\n"
+            + "    builder.innerHTML = '<input type=\"image\">';\n"
+            + "    input = builder.firstChild;\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "<form>\n"
+            + "  <input type='image' id='image1'>\n"
+            + "</form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "-", "-", "-" })
+    public void defaultValuesAfterclone() throws Exception {
+        final String html = "<html><head><title>foo</title>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var input = document.getElementById('image1');\n"
+            + "    input = input.cloneNode(false);\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+
+            + "    input = document.createElement('input');\n"
+            + "    input.type = 'image';\n"
+            + "    input = input.cloneNode(false);\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+
+            + "    var builder = document.createElement('div');\n"
+            + "    builder.innerHTML = '<input type=\"image\">';\n"
+            + "    input = builder.firstChild;\n"
+            + "    input = input.cloneNode(false);\n"
+            + "    alert(input.value + '-' + input.defaultValue);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "<form>\n"
+            + "  <input type='image' id='image1'>\n"
+            + "</form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "initial-initial", "initial-initial", "newValue-newValue", "newValue-newValue",
+                "newDefault-newDefault", "newDefault-newDefault" })
+    public void resetByClick() throws Exception {
+        final String html = "<html><head><title>foo</title>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var image = document.getElementById('testId');\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.getElementById('testReset').click;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.value = 'newValue';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.getElementById('testReset').click;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.defaultValue = 'newDefault';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.forms[0].reset;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "<form>\n"
+            + "  <input type='image' id='testId' value='initial'>\n"
+            + "  <input type='reset' id='testReset'>\n"
+            + "</form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "initial-initial", "initial-initial", "newValue-newValue", "newValue-newValue",
+                "newDefault-newDefault", "newDefault-newDefault" })
+    public void resetByJS() throws Exception {
+        final String html = "<html><head><title>foo</title>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var image = document.getElementById('testId');\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.forms[0].reset;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.value = 'newValue';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.forms[0].reset;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.defaultValue = 'newDefault';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    document.forms[0].reset;\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "<form>\n"
+            + "  <input type='image' id='testId' value='initial'>\n"
+            + "</form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "initial-initial", "default-default", "newValue-newValue", "newDefault-newDefault" })
+    public void defaultValue() throws Exception {
+        final String html = "<html><head><title>foo</title>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var image = document.getElementById('testId');\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.defaultValue = 'default';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+
+            + "    image.value = 'newValue';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+            + "    image.defaultValue = 'newDefault';\n"
+            + "    alert(image.value + '-' + image.defaultValue);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "<form>\n"
+            + "  <input type='image' id='testId' value='initial'>\n"
+            + "</form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+}
